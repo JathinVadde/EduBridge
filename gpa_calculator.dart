6c9ecb898a106054d77ce782eedd48bcc7a6f5e8
JathinVadde <jathin.vv9@gmail.com>
2026-05-31 16:57:50 -0500

    Create gpa_calculator.dart

---
+++ gpa_calculator.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:timeago/timeago.dart' as timeago;
+import '/flutter_flow/custom_functions.dart';
+import '/flutter_flow/lat_lng.dart';
+import '/flutter_flow/place.dart';
+import '/flutter_flow/uploaded_file.dart';
+
+double calculateGPA(
+  double? totalPoints,
+  int? totalCourses,
+) {
+  /// MODIFY CODE ONLY BELOW THIS LINE
+
+  if (totalPoints == null || totalCourses == null || totalCourses <= 0) {
+    return 0.0;
+  }
+
+  return totalPoints / totalCourses;
+
+  /// MODIFY CODE ONLY ABOVE THIS LINE
+}
